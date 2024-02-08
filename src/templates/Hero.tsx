10ec614b7324caab898455b877f44dfb88457704
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2024-02-08 09:59:21 -0500

    Update Hero.tsx

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -11,42 +11,44 @@ import { Logo } from './Logo';
 
 const Hero = () => (
   <Background color="bg-gray-400">
-    <Section yPadding="md:py-0">
-      <NavbarTwoColumns
-        logo={<Logo xl />}
-        // eslint-disable-next-line react/no-children-prop
-        children={
-          <>
-            <li>
-              <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-                GitHub
-              </Link>
-            </li>
-            <li>
-              <Link href="/">Sign in</Link>
-            </li>
-          </>
-        }
-      ></NavbarTwoColumns>
-    </Section>
+    <div className=" bg-[url('https://images.unsplash.com/photo-1558591710-4b4a1ae0f04d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+      <Section yPadding="md:py-0">
+        <NavbarTwoColumns
+          logo={<Logo xl />}
+          // eslint-disable-next-line react/no-children-prop
+          children={
+            <>
+              <li>
+                <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+                  GitHub
+                </Link>
+              </li>
+              <li>
+                <Link href="/">Sign in</Link>
+              </li>
+            </>
+          }
+        ></NavbarTwoColumns>
+      </Section>
 
-    <Section yPadding="py-12">
-      <HeroOneButton
-        title={
-          <>
-            {'Software Engineer\n'}
-            <br />
-            <span className="text-primary-500">at Skillify</span>
-          </>
-        }
-        description=""
-        button={
-          <ScrollLink to="ProjectWork" smooth={true} duration={500}>
-            <Button xl>See My Portfolio</Button>
-          </ScrollLink>
-        }
-      />
-    </Section>
+      <Section yPadding="py-12">
+        <HeroOneButton
+          title={
+            <>
+              {'Lucky Hariharan\n'}
+              <br />
+              <span className="text-primary-500">Software Engineer</span>
+            </>
+          }
+          description=""
+          button={
+            <ScrollLink to="ProjectWork" smooth={true} duration={500}>
+              <Button xl>See My Portfolio</Button>
+            </ScrollLink>
+          }
+        />
+      </Section>
+    </div>
   </Background>
 );
 
