7b526cb401ecd4474e05fca5c72236e002b96257
LuckyHariharan <lharihar@uwaterloo.ca>
2024-02-21 17:34:37 +0530

    Update Hero.tsx

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -10,8 +10,8 @@ import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
 const Hero = () => (
-  <div className="h-screen flex-col  bg-gray-100">
-    <Background color="bg-gray-100">
+  <div className="h-screen flex flex-col bg-[url('https://images.unsplash.com/photo-1558591710-4b4a1ae0f04d?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA')]">
+    <Background>
       <Section yPadding="md:py-0">
         <NavbarTwoColumns
           logo={<Logo xl />}
