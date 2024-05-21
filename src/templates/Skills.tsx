267a6b0b4b14534527e956266fe4da4a4d4c11a1
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2024-05-20 23:43:56 -0400

    Update Skills.tsx
    to have consistent animatiobns with hero

--- src/templates/Skills.tsx
+++ src/templates/Skills.tsx
@@ -1,4 +1,3 @@
-// so I can add images of the skills I use top row being android dev and bottom row being frontend dev
 import { motion, useAnimation } from 'framer-motion';
 import React, { useLayoutEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
@@ -11,8 +10,8 @@ import { SkillsGrid } from './components/SkillGrid';
 interface SectionProps {
   className?: string;
 }
-// complete the sectionprops for this file
-export const Skills = ({ ...rest }: SectionProps) => {
+
+const Skills = ({ ...rest }: SectionProps) => {
   const controls = useAnimation();
   const [ref, inView] = useInView({
     triggerOnce: true,
@@ -21,14 +20,18 @@ export const Skills = ({ ...rest }: SectionProps) => {
 
   useLayoutEffect(() => {
     if (inView) {
-      controls.start({ opacity: 1, transition: { duration: 1 } });
+      controls.start({
+        opacity: 1,
+        y: 0,
+        transition: { duration: 1, ease: 'easeOut' },
+      });
     }
   }, [inView, controls]);
 
   return (
-    <motion.div ref={ref} initial={{ opacity: 0 }} animate={controls}>
+    <motion.div ref={ref} initial={{ opacity: 0, y: 50 }} animate={controls}>
       <Section {...rest}>
-        <div className=" flex w-full flex-col items-center hover:text-slate-500  md:w-full">
+        <div className="flex w-full flex-col items-center hover:text-slate-500 md:w-full">
           <SectionHeader title="Skills" description="" />
           <SkillsGrid />
         </div>
@@ -36,3 +39,5 @@ export const Skills = ({ ...rest }: SectionProps) => {
     </motion.div>
   );
 };
+
+export { Skills };
